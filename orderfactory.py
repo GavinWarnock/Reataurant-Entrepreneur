9799a94e90b471a91d788e7eaf1e5ba0fe6a4d6a
Gavin Warnock <gbwarnock@gmail.com>
2023-03-01 13:04:08 -0700

    Created Factory Pattern in orderfactory.py

--- orderfactory.py
+++ orderfactory.py
@@ -1,5 +1,13 @@
+from pizza import Pizza
+from pasta import Pasta
+from salad import Salad
 class Orderfactory:
 
     @staticmethod
     def create_order(order):
-        pass
+        if order == "Pizza" or order == "pizza":
+            return Pizza()
+        elif order == "Pasta" or order == "pasta":
+            return Pasta()
+        elif order == "Salad" or order == "salad":
+            return Salad()
