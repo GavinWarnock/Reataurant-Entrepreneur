50a653dd1a1fb38b6800688fa517b90dc86c4a9f
Gavin Warnock <gbwarnock@gmail.com>
2023-03-03 08:02:03 -0700

    Finished product

--- logger.py
+++ logger.py
@@ -1,14 +1,14 @@
 
 class Logger:
-    def __init__(self, transaction_count, daily_sales):
-        self.transaction_count = transaction_count
-        self.daily_sales = daily_sales
+    def __init__(self):
+        self.transaction_count = 0
+        self.daily_sales = 0
     
     def log_transaction(self, order, location_number):
         self.transaction_count += 1
         self.daily_sales += order.price
         file = open("log.txt", "a")
-        file.write(f"{self.transaction_count}: {order.dish_name} for store {location_number} for a daily total of ${self.daily_sales}\n")
+        file.write(f"Transaction {self.transaction_count}: {order.dish_name} for store {location_number} for ${order.price} for a daily total of ${self.daily_sales}\n")
         file.close()
 
-logger = Logger(0,0)
+logger = Logger()
--- simulation.py
+++ simulation.py
@@ -5,8 +5,11 @@ class Simulation:
         
         location_one = Franchise(1)
         location_two = Franchise(2)
+        location_three = Franchise (3)
 
+        location_three.place_order()
         location_one.place_order()
         location_two.place_order()
         location_one.place_order()
-        
+        location_three.place_order()
+        location_two.place_order()
